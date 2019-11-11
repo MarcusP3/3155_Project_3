b5713fe41ef9cfe9057d5df907edb99821a3490b
Mmejiaro <mmejiaro@uncc.edu>
2019-11-11 23:45:44 +0000

    Set up the create action

--- projectApp/app/controllers/create_hw_controller.rb
+++ projectApp/app/controllers/create_hw_controller.rb
@@ -1,4 +1,8 @@
 class CreateHwController < ApplicationController
     def new
     end
+    
+    def create
+        render plain: params[:create_hw].inspect
+    end
 end
--- projectApp/config/routes.rb
+++ projectApp/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  resources :schedule
   resources :create_hw
+  resources :schedule
   
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
