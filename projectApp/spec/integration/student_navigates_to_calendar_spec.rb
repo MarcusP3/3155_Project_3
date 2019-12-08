e049d21ca33a1f3dccf29fb96510b20f1e96e0b3
Mmejiaro <mmejiaro@uncc.edu>
2019-12-08 04:32:03 +0000

    Added student navigates to calendar scenario 3 using rspec

---
+++ projectApp/spec/integration/student_navigates_to_calendar_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper.rb'
+
+feature"Student navigates to calendar" do
+    scenario "Student successfully navigates to the calendar from welcome page" do
+        visit root_path
+        expect(page).to have_content("Schedule")
+        expect(page).to have_content("Create Homework")
+        click_link "Schedule"
+        expect(page).to have_content("Schedule")
+        expect(page).to have_content("<")
+        expect(page).to have_content(">")
+        expect(page).to have_content("Sunday")
+    end
+end
