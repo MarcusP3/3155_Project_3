0c3280ce54f808c4dd800badb9204ca820c3a312
Mmejiaro <mmejiaro@uncc.edu>
2019-12-11 22:16:55 +0000

    updated tests

--- projectApp/spec/integration/student_navigates_to_calendar_spec.rb
+++ projectApp/spec/integration/student_navigates_to_calendar_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper.rb'
 feature"Student navigates to calendar" do
     scenario "Student successfully navigates to the calendar from welcome page" do
         visit root_path
-        expect(page).to have_content("Schedule")
+        expect(page).to have_content("schedule")
         expect(page).to have_content("Create Homework")
         click_link "Schedule"
         expect(page).to have_content("Schedule")
